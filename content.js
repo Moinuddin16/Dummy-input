ec85f3b2617bd63cff568cfcc22f7f9ec3d0c318
Moinuddin16 <anik200019@gmail.com>
2020-08-30 02:04:13 +0600

    first commit

---
+++ content.js
@@ -0,0 +1,105 @@
+chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
+    if(request.name == 'body')
+    {
+        console.log(request.name);
+        var allInputs = $(":input");
+        console.log(allInputs);
+        $.each(allInputs,function(index,value) {
+            var type = allInputs[index]["type"];
+            if(type == "color")
+            {
+                allInputs[index]["value"] = '#f70000';
+            }
+            else if(type == "date")
+            {
+                allInputs[index]["value"] = "2020-07-29";
+            }
+            else if(type == "datetime-local")
+            {
+                allInputs[index]["value"] = "2018-06-12T19:30";
+            }
+            else if(type == "email")
+            {
+                allInputs[index]["value"] = 'anik200019@gmail.com';
+            }
+            else if(type == "month")
+            {
+                allInputs[index]["value"] = "2018-05";
+            }
+            else if(type == "number")
+            {
+                allInputs[index]["value"] = '20';
+            }
+            else if(type == "password")
+            {
+                allInputs[index]["value"] = 'anik200019@gmail.com';
+            }
+            else if(type == "radio")
+            {
+                allInputs[index]["checked"] = 'true';
+            }
+            else if(type == "range")
+            {
+                allInputs[index]["value"] = "90";
+            }
+            else if(type == "search")
+            {
+                allInputs[index]["value"] = 'Search';
+            }
+            else if(type == "tel")
+            {
+                allInputs[index]["value"] = "123-456-7890";
+            }
+            else if(type == "text")
+            {
+                allInputs[index]["value"] = 'Text';
+            }
+            else if(type == "time")
+            {
+                allInputs[index]["value"] = "13:30";
+            }
+            else if(type == "url")
+            {
+                allInputs[index]["value"] = 'https://example.com';
+            }
+            else if(type == "week")
+            {
+                allInputs[index]["value"] = '2017-W01';
+            }
+            else if(type == "checkbox")
+            {
+                allInputs[index]["checked"] = 'true';
+            }
+            else if(type == "reset")
+            {
+                allInputs[index]["value"] = 'Reset';
+            }
+            else if(type == "reset")
+            {
+                allInputs[index]["value"] = 'Reset';
+            }
+            else if(type == "select-one")
+            {
+                var options = allInputs[index];
+                $.each(options,function(oindex,ovalue) {  
+                    ovalue["selected"] = 'true';
+                    if(oindex > 0)
+                     return false;
+                });
+            }
+            else if(type == "select-multiple")
+            {
+                var options = allInputs[index];
+                console.log(options);
+                $.each(options,function(oindex,ovalue) {  
+                    if(oindex!=0)
+                    ovalue["selected"] = 'true';
+                    
+                    if(oindex > 1)
+                     return false;
+                });
+            }
+        });
+       // $("body").css("background-color", "yellow");
+    }
+});
---
+++ eventPage.js
@@ -0,0 +1,17 @@
+
+var menuItem = {
+    "id" : "fake_filler",
+    "title" : "Fake Filler",
+    "contexts" : ["all"]
+};
+chrome.contextMenus.create(menuItem);
+
+    chrome.contextMenus.onClicked.addListener(function(clickData){
+        if(clickData.menuItemId == "fake_filler")
+        {
+            chrome.tabs.query({active: true,currentWindow:true},function(tabs){
+                chrome.tabs.sendMessage(tabs[0].id,{name: "body"});
+        });
+    }
+   
+});
